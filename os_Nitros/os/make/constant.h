db5329e3884dbec4c727818db894e74d1d2fba67
Subhajit Sahu <wolfram77@gmail.com>
2014-02-18 15:58:53 +0530

    make folder

---
+++ os_Nitros/os/make/constant.h
@@ -0,0 +1,15 @@
+#ifndef _BUILD_CONSTANT_H_
+#define _BUILD_CONSTANT_H_
+
+
+// Platform
+#define platform_win32		0x0100
+#define platform_avr		0x0101
+
+
+// Compiler
+#define compiler_gcc		0x0200
+#define compiler_visualc	0x0201
+
+
+#endif /* _BUILD_CONSTANT_H_ */
---
+++ os_Nitros/os/make/function.h
@@ -0,0 +1,40 @@
+#ifndef _BUILD_FUNCTION_H_
+#define _BUILD_FUNCTION_H_
+
+
+// Macros: typeOf
+#if compiler_name != compiler_gcc
+	#define typeOf(expr)	\
+	decltype(expr)
+#else
+	#define typeOf(expr)	\
+	typeof(expr)
+#endif
+
+
+// Convert a token to string
+#define toString(a)	\
+#a
+
+
+// Mark unused variables
+#define notUsed(var)	\
+(void)(var)
+
+
+// Apply memory barrier (prevents reordering)
+#ifndef barrier
+	#define barrier()	\
+	asm volatile("" ::: "memory")
+#endif
+
+
+// Assembly coding
+#define assembly	\
+__asm__ __volatile__
+
+#define line(text)	\
+text "\n\t"
+
+
+#endif /* _BUILD_FUNCTION_H_ */
