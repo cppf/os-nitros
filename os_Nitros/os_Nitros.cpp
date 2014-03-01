5cb2ac9e68e63657213d73f79c350239afb30af9
Subhajit Sahu <wolfram77@gmail.com>
2014-03-02 00:01:30 +0530

    a too much increase in code size

--- os_Nitros/os_Nitros.cpp
+++ os_Nitros/os_Nitros.cpp
@@ -1,13 +1,24 @@
 #include "nitros/nitros.hpp"
 using namespace data;
 
-list<uword, 8> List;
+struct bigone
+{
+	long a;
+	long b;
+	long c;
+};
+
+list<bigone, 8> List;
 
 int main(void)
 {
     while(true)
     {
-		List.Add(10);
+		bigone a;
+		a.a = 10;
+		a.b = 11;
+		a.c = 12;
+		List.Add(a);
 		List.DeleteAt(0);
 		*address(&List, 2) = 0;
 	}
