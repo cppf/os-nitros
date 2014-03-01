23deea41b3e9422b7426be4c994b87a1c047477b
Subhajit Sahu <wolfram77@gmail.com>
2014-03-01 10:56:16 +0530

    update

--- os_Nitros/nitros/core/process.hpp
+++ os_Nitros/nitros/core/process.hpp
@@ -1,16 +1,17 @@
-/*
- * process.hpp
- *
- * Created: 1/3/2014 9:45:20 AM
- *  Author: wolfram77
- */ 
-
-
 #ifndef _PROCESS_HPP_
 #define _PROCESS_HPP_
 
+// Status
+#define process_ready		0
+#define process_blocked		1
+#define	process_running		2
 
 
+typedef struct _process
+{
+	void*	Addr;
+	void*	State;
+}process;
 
 
 #endif /* _PROCESS_HPP_ */
