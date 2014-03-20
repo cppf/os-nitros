9db46dea2a5247aa9b75b8dddc34b117660981cf
Subhajit Sahu <wolfram77@gmail.com>
2014-03-20 13:56:57 +0530

    queue test

--- os_Nitros/os_Nitros.cpp
+++ os_Nitros/os_Nitros.cpp
@@ -1,18 +1,18 @@
 #include "nitros/nitros.hpp"
 
-list16<uint, 8> ListU;
-list16<int, 8> ListI;
+queue16<uint, 8> ListU;
+queue16<int, 8> ListI;
 
 int main(void)
 {
 	ListI.Init();
 	ListU.Init();
-	ListI.Add(3);
-	ListI.Add(3);
-	ListI.Add(3);
-	ListU.Add(3);
-	ListU.Add(3);
-	ListU.Add(3);
+	ListI.PushRear(3);
+	ListI.PushRear(3);
+	ListI.PushRear(3);
+	ListU.PushRear(3);
+	ListU.PushRear(3);
+	ListU.PushRear(3);
 /*
 	list_AddF(list_Ptr(ListI), 3);
 	list_AddF(list_Ptr(ListI), 3);
@@ -20,5 +20,5 @@ int main(void)
 	list_AddF(&ListU, 3);
 	list_AddF(&ListU, 3);
 	list_AddF(&ListU, 3);
-	*/
+*/
 }
